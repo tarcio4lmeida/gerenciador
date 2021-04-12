fe493be69b160401adf6c9de7b573d9a3afd7b51
tarcio almeida <tarcioalmeidalima@gmail.com>
2021-04-12 14:01:36 -0300

    Usando a taglib fmt

--- WebContent/listaEmpresas.jsp
+++ WebContent/listaEmpresas.jsp
@@ -3,6 +3,8 @@
 <%@ page
 	import="java.util.List, br.com.alura.gerenciador.servlet.Empresa"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
+
 
 <!DOCTYPE html>
 <head>
@@ -13,10 +15,10 @@
 	Lista de empresas:
 	<br />
 	<ul>
-		<c:forEach items="${empresas}" var="empresa">
-			<li>${empresa.nome}</li>
-		</c:forEach>
-	</ul>
+        <c:forEach items="${empresas}" var="empresa">
+            <li>${empresa.nome } - <fmt:formatDate value="${empresa.dataAbertura}" pattern="dd/MM/yyyy"/> </li>
+        </c:forEach>
+    </ul>
 
 </body>
 </html>
--- src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,6 +1,9 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -18,11 +21,22 @@ public class NovaEmpresaServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");
-		
+	    Date dataAbertura = null;
+
 		String nomeEmpresa = request.getParameter("nome");
+	    String paramDataEmpresa = request.getParameter("data");
+	    
+	    try {
+	    	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			 dataAbertura = sdf.parse(paramDataEmpresa);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	    
 		Empresa empresa = new Empresa();
 		empresa.setNome(nomeEmpresa);
-		
+	    empresa.setDataAbertura(dataAbertura);
+	    
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
